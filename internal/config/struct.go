ce940a86c1034c43a7cba1e51d18a20b18e97dee
白泽平 <bzp2010@apache.org>
2022-02-28 16:03:17 +0800

    feat: add struct comment

--- internal/config/struct.go
+++ internal/config/struct.go
@@ -1,30 +1,30 @@
 package config
 
 type server struct {
-	Host string
-	Port string
+	Host string    `mapstructure:"host"`
+	Port string    `mapstructure:"port"`
 	TLS  serverTLS `mapstructure:"tls"`
 }
 
 type serverTLS struct {
-	Cert string
-	Key  string
+	Cert string `mapstructure:"cert"`
+	Key  string `mapstructure:"key"`
 }
 
 type mysqlConfig struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-	Database string
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	Database string `mapstructure:"database"`
 }
 
 type datasource struct {
-	Type  string
+	Type  string      `mapstructure:"type"`
 	MySQL mysqlConfig `mapstructure:"mysql"`
 }
 
 type config struct {
-	Server     server
+	Server     server     `mapstructure:"server"`
 	DataSource datasource `mapstructure:"datasource"`
 }
