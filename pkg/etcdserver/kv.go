7a8ebeeb48b69a3feeaebaea8fa682e4669caded
Michael Leow <mleow@moneylion.com>
2024-01-05 01:29:14 +0800

    Summary: Add debug for get kv w/ NATS

--- pkg/etcdserver/kv.go
+++ pkg/etcdserver/kv.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/api7/gopkg/pkg/log"
+	"github.com/davecgh/go-spew/spew"
 	"go.etcd.io/etcd/api/v3/etcdserverpb"
 	mvccpb "go.etcd.io/etcd/api/v3/mvccpb"
 	"go.uber.org/zap"
@@ -60,15 +61,34 @@ func (k *EtcdServer) Put(ctx context.Context, r *etcdserverpb.PutRequest) (*etcd
 		rev int64
 		err error
 	)
-	_, kv, _ := k.backend.Get(ctx, key, "0", 0, 0)
+	id, kv, rerr := k.backend.Get(ctx, key, "0", 0, 0)
+
+	if rerr != nil {
+		log.Debugw("GET ERR:", zap.Error(rerr))
+		return nil, rerr
+	}
+
+	log.Debugw("GET ID:", zap.Int64("id", id))
+	spew.Dump(kv)
 
 	if kv != nil {
-		revision, _, _, rerr := k.backend.Update(ctx, key, r.Value, kv.ModRevision, 0)
+		revision, kv, b, rerr := k.backend.Update(ctx, key, r.Value, kv.ModRevision, 0)
+		log.Debugw("UPDATE:", zap.Int64("REV", revision), zap.Bool("OK?", b))
+		spew.Dump(kv)
+		if rerr != nil {
+			log.Debugw("UPDATE ERR:", zap.Error(rerr))
+			return nil, rerr
+		}
 		log.Debugw("update", zap.String("key", key), zap.String("value", string(r.Value)), zap.Int64("revision", revision), zap.Error(rerr))
 		err = rerr
 		rev = revision
 	} else {
 		revision, rerr := k.backend.Create(ctx, key, r.Value, 0)
+		if rerr != nil {
+			log.Debugw("CREATE ERR:", zap.Error(rerr))
+			return nil, rerr
+		}
+		log.Debugw("CREATE:", zap.Int64("REV", revision))
 		log.Debugw("create", zap.String("key", key), zap.String("value", string(r.Value)), zap.Int64("revision", revision), zap.Error(rerr))
 		err = rerr
 		rev = revision
